ab8252d23fcfd8c7daf0766b333c4efff448acca
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 12:07:50 -0600

    People page

--- starfriends/src/App.js
+++ starfriends/src/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
-import './App.css';
+import Home from './components/Home';
+import People from './components/People';
+import Planets from './components/Planets';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container, Dimmer, Loader } from "semantic-ui-react";
 
 function App() {
   const [people, setPeople] = useState([]);
   const [planets, setPlanets] = useState([]);
+  // Loading set to true until data is present
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -12,25 +17,45 @@ function App() {
       let res = await fetch('https://swapi.co/api/people/?format=json');
       let data = await res.json();
       setPeople(data.results);
-      console.log(data.results);
+      setLoading(false);
     }
 
     async function fetchPlanets() {
       let res = await fetch("https://swapi.co/api/planets/?format=json");
       let data = await res.json();
       setPlanets(data.results);
-      console.log(data.results);
+      setLoading(false);
     }
 
     fetchPeople();
     fetchPlanets();
   }, []);
-  console.log('data', people);
 
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Container>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ) : (
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/people">
+                <People data={people}/>
+              </Route>
+              <Route exact path="/planets">
+                <Planets data={planets}/>
+              </Route>
+            </Switch>
+          )}
+        </Container>
+      </Router>
+    </>
   );
 }
 
