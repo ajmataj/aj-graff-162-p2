9b41552bdd48020ea2a597065c5f24715dd3a3db
BuildTools <unconfigured@null.spigotmc.org>
2019-12-18 21:57:55 -0600

    Homepage media and title

--- starfriends/src/components/Home.js
+++ starfriends/src/components/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import logo from '../starwars.png';
 
 export default function Home() {
     return (
-        <div>
-            <p>Home</p>
+        <div class="text-center">
+            <img src={logo} width="400" height="200" alt="Star Wars API"/>
+            <h2>Info about your favorite Star Wars characters, planets, and starships!</h2>
         </div>
     )
 }
